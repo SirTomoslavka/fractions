6dc3ad86ae747de39db5959919a8388e798b5d39
SirTomoslavka <tomasbazant04@gmail.com>
2025-02-10 11:27:36 +0100

    double comparison

--- src/main/java/pro1/Main.java
+++ src/main/java/pro1/Main.java
@@ -3,5 +3,24 @@ package pro1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println(Fraction.IsBigger());
+    }
+}
+
+class Fraction {
+    public static String IsBigger(){
+        double first = 0.3;
+        double second = 0.1+0.2;
+
+        if(first < second){
+            return "0.1 + 0.2 is bigger";
+        }
+        else if(first > second){
+            return "0.3 is bigger";
+        }
+        else {
+            return "Theyre the same";
+        }
     }
 }
